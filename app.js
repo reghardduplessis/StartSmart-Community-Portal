53e668b7cf6e117c39e32a8f0795a14cccc55776
reghardduplessis <regharddupie14@gmail.com>
2025-05-10 15:18:54 +0200

    Added POST functionality to the Contact form in app.js. Also included the header.ejs and footer.ejs in contact.ejs and events.ejs

--- app.js
+++ app.js
@@ -3,15 +3,28 @@
 const express = require("express");
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
-
 const app = express();
 const port = 3000;
+
+// CONTACT FORM CODE - START
+app.use(express.urlencoded({ extended: true }));
+
+const contacts = [];
+
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  contacts.push({ name, email, message });
+  console.log(contacts);
+  res.redirect('/thankyou');
+});
+// CONTACT FORM CODE - END
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use("/", pageRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
