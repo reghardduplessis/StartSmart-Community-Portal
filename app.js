e3554ea5e8a56743188ab4f20c3ced72c7a699e0
Graceyyy <gracekangausaru@gmail.com>
2025-05-12 21:35:24 +0200

    added pictures and about paje contents in about.ejs and app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
+const { name } = require("ejs");
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,16 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+const teammembers = [
+  {name:'Reghard du Plesis', role:'', image: '/images/reggie.jpg'},
+  {name:'Chad Clifton', role:'Team Leader', image: '/images/chad.jpg'},
+  {name:'Zander Lindeque', role:'Data ', image: '/images/zander.jpg'},
+  {name:'Christopher Wawa', role:'Frontend Developer', image: '/images/chris.jpg'},
+  {name:'Grace Kangausaru', role:'Backend Developer', image: '/images/grace.jpg'},
+];
+
+app.locals.team =teammembers;
 
 app.use("/", pageRoutes);
 
