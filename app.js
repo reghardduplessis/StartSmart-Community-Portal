ad25e6df1f157da0f9c4e53b16eb3ab9db15912d
Christopher-Vhs <christopherwawa395@gmail.com>
2025-05-13 18:19:24 +0200

    WIP: saving local changes before pull

--- app.js
+++ app.js
@@ -16,3 +16,5 @@ app.use("/", pageRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,8 +4,15 @@ const express = require('express');
 const router = express.Router();
 const { events } = require('../public/data/events');
 
+
+// Home page route
+
+
 router.get('/', (req, res) => {
-    res.render('./pages/home');
+    res.render('./pages/home',{
+    title: 'Welcome to Our Vibrant and Ever-Growing Community Portal!',
+    welcomeParagraph: 'We are absolutely thrilled to have you join us on this exciting journey of connection, collaboration, and celebration! Dive into a rich and engaging experience where you can explore a diverse array of upcoming events, discover the inspiring stories and passion behind our dedicated team, and stay fully immersed in all the dynamic happenings within our bustling community. Whether you’re here to learn something new, forge meaningful connections, share your unique talents, or simply soak in the energy of our collective spirit, there’s a place for everyone in our inclusive hub. Get ready to engage, contribute, and be a part of something truly special. Join us today and let’s build an unforgettable community experience together!'
+    });
 });
 
 router.get('/about', (req, res) => {
