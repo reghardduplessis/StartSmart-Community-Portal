394aa629374e69ac7b46d0f680f9e7c716676ad1
zannlin <zanderlindeque1@gmail.com>
2025-05-13 12:46:23 +0200

    Updated Nav bar to look more nicer

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,23 +5,23 @@ const router = express.Router();
 const { events } = require('../public/data/events');
 
 router.get('/', (req, res) => {
-    res.render('./pages/home');
+    res.render('./pages/home', { title: "Home" });
 });
 
 router.get('/about', (req, res) => {
-    res.render('./pages/about', {team: req.app.locals.team});
+    res.render('./pages/about', {team: req.app.locals.team, title: "About" });
 });
 
 router.get('/events', (req, res) => {
-    res.render('./pages/events', { events });
+    res.render('./pages/events', { events ,title: "Events" });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('./pages/contact');
+    res.render('./pages/contact', { title: "Contact" });
 });
 
 router.get('/thankyou', (req, res) => {
-    res.render('./pages/thankyou');
+    res.render('./pages/thankyou', { title: "Thank you" });
 });
 
 module.exports = router;
