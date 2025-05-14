a7fb3d44bf66028b45cc4a2f6e3e6f9f6652dbbc
Chad Clifton <chadcvm@gmail.com>
2025-05-14 13:04:39 +0200

    Moved team array from app.js into data/about.js

---
+++ public/data/about.js
@@ -0,0 +1,11 @@
+//creating array to store team members name, role and image
+const teamMembers = [
+  {name:'Reghard du Plesis', role:'Documentation Manager', image: '/images/about/reggie.jpg'},
+  {name:'Chad Clifton', role:'Team Leader', image: '/images/about/chad.jpg'},
+  {name:'Zander Lindeque', role:'Data Manager', image: '/images/about/zander.jpg'},
+  {name:'Christopher Wawa', role:'Frontend Developer', image: '/images/about/chris.jpg'},
+  {name:'Grace Kangausaru', role:'Backend Developer', image: '/images/about/grace.jpg'},
+  {name:'Bootstrap', role:'Frontend Coordinator', image:'/images/about/bootstrap.jpeg'}
+];
+
+exports.teamMembers = teamMembers;
